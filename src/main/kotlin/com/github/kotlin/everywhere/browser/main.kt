5e2e3a1b22387bd274b80ffcd026c2bc3c3844de
Sung Min Park <vamf12@Sungs-MacBook-Pro.local>
2017-07-24 15:02:36 +0900

    Event Handler 추가

--- src/main/kotlin/com/github/kotlin/everywhere/browser/main.kt
+++ src/main/kotlin/com/github/kotlin/everywhere/browser/main.kt
@@ -2,6 +2,7 @@ package com.github.kotlin.everywhere.browser
 
 import com.github.kotlin.everywhere.browser.Snabbdom.h
 import org.w3c.dom.Element
+import org.w3c.dom.events.Event
 
 sealed class Cmd<out S> {
     companion object {
@@ -26,6 +27,10 @@ sealed class Html<out S> {
         fun <S> input(attributes: List<Attribute<S>>, children: List<Html<S>>): Html<S> {
             return Element("input", attributes, children)
         }
+
+        fun <S> button(attributes: List<Attribute<S>>, children: List<Html<S>>): Html<S> {
+            return Element("button", attributes, children)
+        }
     }
 
     internal class Text<out S>(val text: String) : Html<S>()
@@ -33,8 +38,9 @@ sealed class Html<out S> {
 }
 
 sealed class Attribute<out S> {
-    internal class TextProperty<out S>(val name: String, val value: String) : Attribute<S>()
-    internal class BooleanProperty<out S>(val name: String, val value: Boolean) : Attribute<S>()
+    class TextProperty<out S>(val name: String, val value: String) : Attribute<S>()
+    class BooleanProperty<out S>(val name: String, val value: Boolean) : Attribute<S>()
+    class EventHandler<out S>(val name: String, val value: (Event) -> S) : Attribute<S>()
 
     companion object {
         fun <S> class_(class_: String): Attribute<S> {
@@ -44,35 +50,43 @@ sealed class Attribute<out S> {
         fun <S> disabled(disabled: Boolean): Attribute<S> {
             return BooleanProperty("disabled", disabled)
         }
+
+        fun <S> onClick(msg: S): Attribute<S> {
+            return EventHandler("click") { msg }
+        }
     }
 }
 
-private fun <S> List<Attribute<S>>.toProps(): dynamic {
+private fun <S> List<Attribute<S>>.toProps(receiver: (S) -> Unit): Pair<dynamic, dynamic> {
     val props: dynamic = object {}
+    val on: dynamic = object {}
+
     this.forEach { attr ->
         when (attr) {
             is Attribute.TextProperty -> props[attr.name] = attr.value
             is Attribute.BooleanProperty -> props[attr.name] = attr.value
+            is Attribute.EventHandler -> on[attr.name] = { event: Event -> receiver(attr.value(event)) }
         }
     }
-    return props
+
+    return Pair(props, on)
 }
 
-private fun <S> Html<S>.toVirtualNode(): dynamic {
+private fun <S> Html<S>.toVirtualNode(receiver: (S) -> Unit): dynamic {
     return when (this) {
         is Html.Text -> this.text
         is Html.Element -> {
             val data: dynamic = object {}
-            if (this.attributes.isNotEmpty()) {
-                data["props"] = this.attributes.toProps()
-            }
+            val (props, on) = this.attributes.toProps(receiver)
+            data["props"] = props
+            data["on"] = on
             h(
                     this.tagName,
                     data,
                     this.children
                             .map {
                                 @Suppress("UnsafeCastFromDynamic")
-                                it.toVirtualNode()
+                                it.toVirtualNode(receiver)
                             }
                             .toTypedArray()
             )
@@ -84,11 +98,22 @@ class Program<M, S>(private val container: Element, init: M,
                     private val update: (S, M) -> Pair<M, Cmd<S>>, private val view: (M) -> Html<S>, onAfterRender: ((Element) -> Unit)?) {
 
     private var model = init
-    private var vNode = h("div", view(model).toVirtualNode())
+    private val receiver: (S) -> Unit = { msg ->
+        val (newModel, _) = update(msg, model)
+        if (newModel != model) {
+            model = newModel
+            updateView()
+        }
+    }
+    private var virtualNode = h("div", view(model).toVirtualNode(receiver))
     private val patch = if (onAfterRender != null) Snabbdom.init({ onAfterRender(container) }) else Snabbdom.init(null)
 
     init {
-        patch(container, vNode)
+        patch(container, virtualNode)
+    }
+
+    private fun updateView() {
+        virtualNode = patch(virtualNode, h("div", view(model).toVirtualNode(receiver)))
     }
 }
 
--- src/test/kotlin/com/github/kotlin/everywhere/browser/testView.kt
+++ src/test/kotlin/com/github/kotlin/everywhere/browser/testView.kt
@@ -2,8 +2,11 @@ package com.github.kotlin.everywhere.browser
 
 import com.github.kotlin.everywhere.browser.Attribute.Companion.class_
 import com.github.kotlin.everywhere.browser.Attribute.Companion.disabled
+import com.github.kotlin.everywhere.browser.Attribute.Companion.onClick
+import com.github.kotlin.everywhere.browser.Html.Companion.button
 import com.github.kotlin.everywhere.browser.Html.Companion.div
 import com.github.kotlin.everywhere.browser.Html.Companion.input
+import com.github.kotlin.everywhere.browser.Html.Companion.text
 import com.github.kotlin.everywhere.ktqunit.asyncTest
 import com.github.kotlin.everywhere.ktqunit.fixture
 import org.junit.Test
@@ -12,15 +15,19 @@ import kotlin.test.assertEquals
 
 
 class TestView {
-    private class Model
-    private sealed class Msg
+    private data class Model(val clicked: Boolean = false)
+    private sealed class Msg {
+        object Clicked : Msg()
+    }
 
     private val init = Model()
 
-    private val update: (Msg, Model) -> Pair<Model, Cmd<Msg>> = { _, model ->
-        model to Cmd.none<Msg>()
+    private val update: (Msg, Model) -> Pair<Model, Cmd<Msg>> = { msg, model ->
+        val newModel = when (msg) {
+            Msg.Clicked -> model.copy(clicked = true)
+        }
+        newModel to Cmd.none<Msg>()
     }
-
 
     @Test
     fun testTextProperty() {
@@ -56,6 +63,41 @@ class TestView {
             runProgram(container, init, update, view) {
                 assertEquals("<input disabled=\"\"><input>", root().children().first().html())
                 resolve(Unit)
+            }
+        }
+    }
+
+    @Test
+    fun testEventHandler() {
+        val view: (Model) -> Html<Msg> = { (clicked) ->
+            button(listOf(onClick(Msg.Clicked)), listOf(text(if (clicked) "clicked" else "")))
+        }
+
+        val (container, root) = prepareFixture()
+
+        asyncSerialTest(container, view,
+                {
+                    assertEquals("<button></button>", root().html())
+                    root().children().first().click()
+                    Unit
+                },
+                {
+                    assertEquals("<button>clicked</button>", root().html())
+                }
+        )
+    }
+
+    private fun asyncSerialTest(container: Element, view: (Model) -> Html<Msg>, vararg tests: () -> Unit) {
+        val lefts = tests.toMutableList()
+        return asyncTest { resolve, _ ->
+            runProgram(container, init, update, view) {
+                val test = lefts[0]
+                lefts.removeAt(0)
+                test()
+
+                if (lefts.isEmpty()) {
+                    resolve(Unit)
+                }
             }
         }
     }
