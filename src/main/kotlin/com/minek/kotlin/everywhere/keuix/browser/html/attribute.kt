336ae51b049182a3bcaced8d877b016985dfab11
DESKTOP-S4T0JA8\JIYEON <jiyeon@mine-k.co.kr>
2017-08-10 17:41:14 +0900

    책임님 dynamic attribute 랑 snabbdom 버전 변경 등등

--- src/main/kotlin/com/minek/kotlin/everywhere/keuix/browser/html/attribute.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/keuix/browser/html/attribute.kt
@@ -2,9 +2,16 @@ package com.minek.kotlin.everywhere.keuix.browser.html
 
 
 fun <S> class_(class_: String): Attribute<S> {
-    return Attribute.TextProperty("className", class_)
+    return Attribute.TextProperty("class", class_)
 }
 
+fun <S> dynamic(name: String, value :String): Attribute<S> {
+    return Attribute.TextProperty(name, value)
+}
+
+fun <S> dataset(name: String, value :String): Attribute<S> {
+    return Attribute.DatasetProperty(name, value)
+}
 fun <S> disabled(disabled: Boolean): Attribute<S> {
     return Attribute.BooleanProperty("disabled", disabled)
 }
@@ -17,8 +24,4 @@ fun <S> value(value: String): Attribute<S> {
 
 fun <S> style(style: String): Attribute<S> {
     return Attribute.TextProperty("style", style)
-}
-
-fun <S> dynamic(name: String, value :String): Attribute<S> {
-    return Attribute.TextProperty(name, value)
 }
